e3a74070de2d09d756f8a9fe7b8af2cc28fa0d2b
pedrocpacheco <cpedropacheco@hotmail.com>
2024-02-28 11:55:13 -0300

    feat: add aula 02-28

--- src/main/java/br/com/fiap/cashflowpro/CahsflowproApplication.java
+++ src/main/java/br/com/fiap/cashflowpro/CahsflowproApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.cashflowpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
 public class CahsflowproApplication {
 
 	public static void main(String[] args) {
